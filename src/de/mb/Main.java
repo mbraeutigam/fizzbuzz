49f2d195967945e6b4379ef1fe7c5f345d89357c
Marc Bräutigam <marc.braeutigam@novasib.de>
2020-07-24 11:25:47 +0200

    Update comment

--- src/de/mb/Main.java
+++ src/de/mb/Main.java
@@ -2,18 +2,18 @@ package de.mb;
 
 public class Main {
 	public static void main(final String[] args) {
-		// Die Spieler sitzen in der Regel im Kreis. Der als erste bestimmte
-		// Spieler sagt die Zahl „1“, und jeder Spieler zählt von da an reihum
-		// eine Zahl weiter. Jede durch drei teilbare Zahl wird jedoch durch das
-		// Wort „fizz“ und jede durch fünf teilbare Zahl durch das Wort „buzz“
-		// ersetzt. Durch 15 teilbare Zahlen werden zu „fizz buzz“. Ein Spieler,
-		// der zögert oder einen Fehler macht, scheidet aus dem Spiel aus.
-		// Eine typische Runde Fizz Buzz würde beispielsweise wie folgt
-		// beginnen:
+		// Players generally sit in a circle. The player designated to go first
+		// says the number "1", and the players then count upwards in turn.
+		// However, any number divisible by three is replaced by the word fizz
+		// and any number divisible by five by the word buzz. Numbers divisible
+		// by 15 become fizz buzz. A player who hesitates or makes a mistake is
+		// eliminated from the game.
 		//
-		// Ergebnis: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13,
-		// 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26,
-		// Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz …
+		// For example, a typical round of fizz buzz would start as follows:
+		//
+		// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz
+		// Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28,
+		// 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...
 
 		for (int i = 1; i <= 100; i++) {
 			if (i % 15 == 0) {
